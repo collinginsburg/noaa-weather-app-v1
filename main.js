5e418970eb7f424b478b43f0d460822e6771fd0e
collinginsburg <55665212+collinginsburg@users.noreply.github.com>
2023-11-14 11:04:17 -0500

    Add fallback function if weather station is showing null value for weather

--- main.js
+++ main.js
@@ -20,8 +20,38 @@ if ('geolocation' in navigator){
                         return response.json();
                     }).then(function (obj){
                         console.log(obj); //console.log object with all current weather condition for stations!
-                        let currentTemperature = +(1.8*(obj.properties.temperature.value)+32).toFixed(0);
-                        document.querySelector('.temperature').innerHTML = `${currentTemperature}&deg`;
+                        let currentTemperature = obj.properties.temperature.value;
+
+                        // fallback if station is showing null values
+                        if (currentTemperature === null){ 
+                            console.log(`the current temp is at ${stationID} is ${currentTemperature}`);
+                            fetch(`${urlObservationStations}`).then(function (response){ // fetch list of staions URL, create json object
+                                return response.json();
+                            }).then(function (obj){
+                                let stationID = obj.features[1].properties.stationIdentifier; //choose second station in list of stations, get its ID
+                                let currentWeatherURL = `https://api.weather.gov/stations/${stationID}/observations/latest?require_qc=false` //create current weather url with station id
+                                fetch(`${currentWeatherURL}`).then(function (response){ //fetch url for current weather, create json object
+                                    return response.json();
+                                }).then(function (obj){
+                                    console.log(obj);
+                                    currentTemperature = obj.properties.temperature.value;
+
+                                    currentTemperature = +(1.8*(currentTemperature)+32).toFixed(0);
+                                    console.log(`the temperature at ${stationID} is ${currentTemperature}`);
+
+                                    document.querySelector('.temperature').innerHTML = `${currentTemperature}&deg`;
+                                }
+                                )
+                            }
+                        )} else { //if current temp is not null at the original station
+
+                            currentTemperature = +(1.8*(currentTemperature)+32).toFixed(0);
+                            console.log(`the temperature at ${stationID} is ${currentTemperature}`);
+
+                            document.querySelector('.temperature').innerHTML = `${currentTemperature}&deg`;
+                        };
+
+
                     })
                 })
             })
